a43c26d8b272bed4fb88eb4c1a9b2c2f3931c394
Michel Silva <silva.michel.b@gmail.com>
2022-03-01 01:31:20 -0300

    create tracking route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TrackController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -27,3 +28,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/tracking', [
+    TrackController::class, 'index'
+])->name('tracking');
