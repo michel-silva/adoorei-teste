95dca84329d1611667c003d70b2a3dd53f4b9cec
Michel Silva <silva.michel.b@gmail.com>
2022-03-02 00:29:31 -0300

    update Track model

--- app/Models/Track.php
+++ app/Models/Track.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Track extends Model
 {
@@ -12,6 +14,46 @@ class Track extends Model
     protected $fillable = [
         'tracking_number',
         'history',
-        'status'
+        'status',
+        'last_update',
+        'user_id'
     ];
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    protected static function booted()
+    {
+        static::creating(function ($track) {
+            $track->user_id = Auth::id();
+        });
+    }
+
+    protected static function boot() {
+        parent::boot();
+        static::addGlobalScope('test', function(Builder $builder) {
+           $builder->where('user_id', Auth::id());
+        });
+    }
+
+
+    static function getStatusTypes()
+    {
+        return [
+            '' => 'Aguardando Postagem',
+            'Objeto postado' => 'Postado',
+            'Objeto em trânsito - por favor aguarde' => 'Em trânsito',
+            'Objeto aguardando retirada no endereço indicado' => 'Retirada',
+            'Objeto saiu para entrega ao destinatário' => 'Saiu para Entrega',
+            'Objeto entregue ao destinatário' => 'Entregue',
+            'Objeto devolvido ao país de origem' => 'Devolvido'
+        ];
+    }
+
+    protected $cast = [
+        'history' => 'array'
+    ];
+
 }
