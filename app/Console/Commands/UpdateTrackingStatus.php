5a99eba0828961481d5dce7a46c675dc6908cb07
Michel Silva <silva.michel.b@gmail.com>
2022-03-02 00:30:19 -0300

    create command UpdateTrackingStatus

---
+++ app/Console/Commands/UpdateTrackingStatus.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Track;
+use App\Support\Getaway\Correios;
+use Illuminate\Console\Command;
+
+class UpdateTrackingStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:trackingStatus';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update tracking status of each tracking code.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $tracks = Track::get();
+        $correios = new Correios();
+
+        foreach ($tracks as $track) {
+            $data = $correios->getDataWebServer($track->tracking_number);
+            Track::find($track->id)->update($data);
+        }
+
+        return 0;
+    }
+}
