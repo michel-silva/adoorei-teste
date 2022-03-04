dccaf4a084c2c053b2b785fd44ac1e9fb39fa946
Michel Silva <silva.michel.b@gmail.com>
2022-03-03 23:13:02 -0300

    change file name and implement dashboardDetail

--- resources/js/Pages/Dashboard/DashboardIndex.vue
+++ resources/js/Pages/Dashboard/DashboardIndex.vue
@@ -9,7 +9,7 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                    <DashboardDetail :countTracksGroup="countTracksGroup" :totalTracks="totalTracks" :statusTypes="statusTypes"/>
                 </div>
             </div>
         </div>
@@ -20,11 +20,14 @@
     import { defineComponent } from 'vue'
     import AppLayout from '@/Layouts/AppLayout.vue'
     import Welcome from '@/Jetstream/Welcome.vue'
+    import DashboardDetail from '@/Pages/Dashboard/DashboardDetail.vue'
 
     export default defineComponent({
         components: {
             AppLayout,
             Welcome,
+            DashboardDetail,
         },
+        props: ['countTracksGroup', 'totalTracks', 'statusTypes']
     })
 </script>
