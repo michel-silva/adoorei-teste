c9af2eae10d8778d084f76c7f4af4101d09d2f61
Michel Silva <silva.michel.b@gmail.com>
2022-03-03 23:14:08 -0300

    change file name

---
+++ resources/js/Pages/Tracking/TracksIndex.vue
@@ -0,0 +1,135 @@
+<template>
+  <app-layout title="RASTREAMENTO DE ENCOMENDAS">
+    <!-- <div class="w-1/4 py-8 bg-slate-200">
+      <img class="object-fill mx-auto px-4" src="/images/app/logo-header.svg" />
+    </div> -->
+    <template #header>
+      <div class="flex justify-between">
+        <div class="px-4 py-3">
+          <div class="text-xl">RASTREAMENTO DE ENCOMENDAS</div>
+          <div class="text-sm">
+            Acompanhe seus pacotes enviados em tempo real
+          </div>
+        </div>
+        <div class="py-3">
+          <jet-button @click="openModalTracking()">
+            Cadastrar Código
+          </jet-button>
+          <jet-button
+            class="lg:ml-2 md:mt-2 sm:mt-2"
+            @click="updateTrackingCodes()"
+          >
+            Atualizar Códigos
+          </jet-button>
+        </div>
+      </div>
+    </template>
+
+    <div class="py-2">
+      <div class="mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white shadow-xl sm:rounded-lg">
+          <div class="bg-slate-50 p-4">
+            <tracks-list :tracks="tracks" :statusTypes="statusTypes" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-layout>
+
+  <!-- Modal add tracking number-->
+  <jet-dialog-modal :show="isTrackingModalOpen" @close="closeModalTracking">
+    <template #title>
+      <h3 class="text-gray-700 text-md font-bold">CADASTRAR CÓDIGO</h3>
+      <h4 class="text-sm">
+        Informe o código de rastreio que deseja monitorar.
+      </h4>
+    </template>
+
+    <template #content>
+      <input
+        type="text"
+        class="
+          shadow
+          appearance-none
+          border
+          rounded
+          w-full
+          py-2
+          px-3
+          text-gray-700
+          leading-tight
+          focus:outline-none focus:shadow-outline
+        "
+        id="tracking_number"
+        placeholder="Digite o Código"
+        v-model="form.tracking_number"
+      />
+    </template>
+
+    <template #footer>
+      <jet-secondary-button @click="closeModalTracking">
+        Cancelar
+      </jet-secondary-button>
+
+      <jet-button
+        class="ml-3"
+        @click="saveTrackingCode"
+        :class="{ 'opacity-25': form.processing }"
+        :disabled="form.processing"
+      >
+        Monitorar
+      </jet-button>
+    </template>
+  </jet-dialog-modal>
+</template>
+
+
+<script>
+import { defineComponent } from "vue"
+import AppLayout from "@/Layouts/AppLayout.vue"
+import JetDialogModal from "@/Jetstream/DialogModal.vue"
+import JetSecondaryButton from "@/Jetstream/SecondaryButton.vue"
+import JetButton from "@/Jetstream/Button.vue"
+import TracksList from "@/Pages/Tracking/TracksList.vue"
+
+export default defineComponent({
+  components: {
+    AppLayout,
+    JetDialogModal,
+    JetSecondaryButton,
+    JetButton,
+    TracksList,
+  },
+  props: ["tracks", "statusTypes"],
+  data() {
+    return {
+      isTrackingModalOpen: false,
+      form: this.$inertia.form({
+        tracking_number: "",
+      }),
+    };
+  },
+  methods: {
+    updateTrackingCodes() {
+      this.$inertia.post("/tracking/updateTracks");
+    },
+    openModalTracking() {
+      this.isTrackingModalOpen = true;
+    },
+    closeModalTracking() {
+      this.isTrackingModalOpen = false;
+      this.resetForm();
+    },
+    saveTrackingCode() {
+      this.$inertia.post("/tracking", this.form);
+      this.resetForm();
+      this.closeModalTracking();
+    },
+    resetForm() {
+      this.form = {
+        tracking_number: null,
+      };
+    },
+  },
+});
+</script>
