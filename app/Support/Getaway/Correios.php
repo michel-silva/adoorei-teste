5458b0095eaf8a85c8f1f2b5d4bbf8b00aa6ff82
Michel Silva <silva.michel.b@gmail.com>
2022-03-03 23:06:30 -0300

    refactoring helper Correios

--- app/Support/Getaway/Correios.php
+++ app/Support/Getaway/Correios.php
@@ -23,23 +23,24 @@ class Correios {
     public function getDataWebServer($tracking_number)
     {
 
-        $return = $this->Request($tracking_number);
+        $returnWebServer = $this->Request($tracking_number);
         $status = Track::getStatusTypes();
 
         $data['tracking_number'] = $tracking_number;
 
-        if ( isset($return->objetos[0]->eventos) ) {
-            $data['history'] = json_encode($return->objetos[0]->eventos);
+        if ( isset($returnWebServer->objetos[0]->eventos) ) {
+            $data['events'] = $returnWebServer->objetos[0]->eventos;
 
-            $descricao = $return->objetos[0]->eventos[0]->descricao;
-            $data_hora = new DateTime($return->objetos[0]->eventos[0]->dtHrCriado);
+            $descricao = $returnWebServer->objetos[0]->eventos[0]->descricao;
+            $data_hora = new DateTime($returnWebServer->objetos[0]->eventos[0]->dtHrCriado);
 
             $data['last_update'] = $data_hora->format('d/m/Y - h:m') .' - ' .$descricao;
 
             //when statusType is not defined in Track::getStatusTypes the field $descricao is set
-            $data['status'] = ( isset($status[$descricao]) ) ? $status[$descricao] : $descricao;
+            $data['status'] = ( isset($status[$descricao]) ) ? $status[$descricao]['short_status'] : $descricao;
 
         } else {
+            $data['events'] = [];
             $data['last_update'] = 'Aguardando Postagem';
             $data['status'] = 'Aguardando Postagem';
 
